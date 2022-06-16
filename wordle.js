4fe58c3872f1d44305cdb33a57d27f71b5b441f5
tomtrxn <tomtrann3005@gmail.com>
2022-06-16 19:15:05 +1000

    Commit 5 - Tricky Arrays
    Basing off wordle logic, figured that I needed a nested array within array and various functions to store words/letters.

--- wordle.js
+++ wordle.js
@@ -2,16 +2,31 @@
 document.addEventListener("DOMContentLoaded", () => {
     createSquares();
     
+    //array within array - an array will contain all word guesses, and each word guess will contain an array of letters
+    const guessedWord = [[]]
+
     //now need to figure out how to iterate over each key and the onclick handler
     const keys = document.querySelectorAll('.keyboard-row button');
-
+    
     for (let i = 0; i < keys.length; i++) {
     keys[i].onclick = ({target}) => {
         const key = target.getAttribute('data-key');
-
-        console.log(key);
+        updateGuessedWord(key)
+        //console.log(key); - quick console log check to see if it reflecting
+    };
+}      
+    //function to tell us the number of words guessed so far
+    function getCurrentWordArr() {
+        const numberOfGuessedWords = guessedWord.length;
+        return guessedWord[numberOfGuessedWords - 1];
     }
-    
+
+    //function to tell us the number of letters in the current word - limit of 5
+    function updateGuessedWord(letter) {
+        const currentWordArr = getCurrentWordArr();
+        if(currentWordArr && currentWordArr.length < 5) {
+            currentWordArr.push(letter);
+        }
     }
     
 
