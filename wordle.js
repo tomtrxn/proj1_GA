2aa00f8cc657ffe8c9c3e3a46ab7dfa9f137e965
tomtrxn <tomtrann3005@gmail.com>
2022-06-16 18:51:09 +1000

    Commit 4 - Keyboard and iteration!
    Finally figured out how to make keyboard visible + some nice styling :O. Also was able to iterate the key inputs into console. Now need to figure out how to reflect on the actual wordle tiles...

--- wordle.js
+++ wordle.js
@@ -1,10 +1,23 @@
+// Below ↓ https://developer.mozilla.org/en-US/docs/Web/API/Document/DOMContentLoaded_event - little optimisation for faster loading/parsing
 document.addEventListener("DOMContentLoaded", () => {
-
     createSquares();
+    
+    //now need to figure out how to iterate over each key and the onclick handler
+    const keys = document.querySelectorAll('.keyboard-row button');
+
+    for (let i = 0; i < keys.length; i++) {
+    keys[i].onclick = ({target}) => {
+        const key = target.getAttribute('data-key');
+
+        console.log(key);
+    }
+    
+    }
+    
 
     //function to create 5x6 grid for wordle
     function createSquares() {
-        const gameBoard = document.getElementById("board")
+        const gameBoard = document.getElementById("board");
 
         for (let index = 0; index < 30; index++) {
             let square = document.createElement("div");
@@ -13,4 +26,4 @@ document.addEventListener("DOMContentLoaded", () => {
             gameBoard.appendChild(square);
         } 
     }
-})
+});
